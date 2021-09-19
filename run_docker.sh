2753f1a8d01ad04e4eaa0b834431a4adaa088811
Ben <ben.pfirrmann@freenet.de>
2021-09-19 22:22:00 +0000

    updating run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -3,10 +3,14 @@
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
-# Build image and add a descriptive tag
+docker build --tag=mlapp
 
 # Step 2: 
 # List docker images
+# https://docs.docker.com/engine/reference/commandline/images/
+docker images --all
 
 # Step 3: 
 # Run flask app
+# Container port 80, make-preduction port 8000 (which uses the running app)
+docker run -p 8000:80 mlapp
