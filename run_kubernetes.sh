79f339215ea6b992174e209f87ceec9675b98572
EC2 Default User <ec2-user@ip-172-31-45-28.ec2.internal>
2021-10-01 19:14:16 +0000

    adding first version of run_kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,22 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="bencdr0/mlapponline"
+
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run mlapp\
+    #--generator=run-pod/v1\ # outdated, no longer needed
+    --image=$dockerpath\
+    --port=80 --labels app=mlapp
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
+
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward mlapp 8000:80
 
