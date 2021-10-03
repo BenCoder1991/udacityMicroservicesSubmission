02c80d037b29485af6829bee131d6450100e3fe3
EC2 Default User <ec2-user@ip-172-31-33-26.ec2.internal>
2021-10-03 11:07:35 +0000

    update cloud9 setup

--- cloud9_setup_script.sh
+++ cloud9_setup_script.sh
@@ -1,13 +1,20 @@
 # This script is written to accelerate and standardized the prescribed setup in cloud9 envrionments 
 # (specs: t3.medium, amazon linux, resizing hard disk to 20 GB)
+python3 -m venv ~/.devops
+source ~/.devops/bin/activate
+
+# Cloud 9 only: re-using the standard re-size script to increase hard disk space
+# Reference: https://docs.aws.amazon.com/cloud9/latest/user-guide/move-environment.html
+./resize.sh
+
+make install
 
 # setup hadolint (sudo chmod in order to make it running on cloud9) - this circumvents "sudo" issues within circleci
 sudo wget -O /bin/hadolint https://github.com/hadolint/hadolint/releases/download/v1.16.3/hadolint-Linux-x86_64 
 sudo chmod +x /bin/hadolint
 
-# Cloud 9 only: re-using the standard re-size script to increase hard disk space
-# Reference: https://docs.aws.amazon.com/cloud9/latest/user-guide/move-environment.html
-./resize.sh
+# running the linting checks
+make lint
 
 # Installing minikube according to standard documentation:
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
@@ -21,6 +28,7 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kubectl version --client
 
 # afterwards proceed as described in the readme file.
+# don't forget to activate the virtual environmnet after exiting
 
 
 
